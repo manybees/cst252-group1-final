46cf98288ea9d5fc2a2026bbf49cb2d22bb6eaf7
mgubanova <70294900+mgubanova@users.noreply.github.com>
2020-10-30 17:41:28 -0700

    pseudocode

---
+++ js/site.js
@@ -0,0 +1,17 @@
+Intro GIF + audio (Title & doors fade in)
+
+Doors number TBD
+Every door: multiple images (open/closed); animation
+Each door = button
+Library of pages + animation (3 random pages per door)
+Page content
+Portfolio pieces - 6
+Random images, videos, gifs - 12
+
+
+// load page - intro animation
+// load page - doors closed + Title
+// door open on click + animation plays
+// delay
+// take into a random page contents (for loop)
+//
